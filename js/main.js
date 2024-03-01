26fab92f96945e6b203bdcc974e4de4ad4ce4ffd
Kataru <belogorod5@gmail.com>
2024-03-01 15:51:51 +0200

    add pages

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//SLIDER
+$(document).ready(function() {
+    $('.slider').slick({
+        dots: true,
+    });
+});
+
+// TABS
+const tabsBtn = document.querySelectorAll(".promo__btn-tabs");
+const tabsItems = document.querySelectorAll(".promo__afisha-page");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+    item.addEventListener("click", function() {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId);
+
+        if( ! currentBtn.classList.contains('active') ) {
+            tabsBtn.forEach(function(item) {
+                item.classList.remove('active');
+            });
+    
+            tabsItems.forEach(function(item) {
+                item.classList.remove('active');
+            });
+    
+            currentBtn.classList.add('active');
+            currentTab.classList.add('active');
+        }
+
+    });
+}
+
+document.querySelector('.promo__btn-tabs').click();
